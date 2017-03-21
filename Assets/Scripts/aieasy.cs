f675451442b51ad43fc56bafed7ae7e7205ede17
Joe <Prochaska.joseph@gmail.com>
2017-03-20 23:41:10 -0400

    Removed script / editing player/enemy
    Removed redunant oncollison script and added it to the AI script. Also added some cubes to the player and enemy to siginify forward motion.

--- Assets/Scripts/aieasy.cs
+++ Assets/Scripts/aieasy.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class aieasy : MonoBehaviour {
+public class aieasy : MonoBehaviour
+{
 
     public float fpsTargetDistance;
     public float enemyLookDistance;
@@ -14,46 +15,106 @@ public class aieasy : MonoBehaviour {
     Rigidbody theRigidBody;
     Renderer myRender;
 
+    private bool didRespawn;
+
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         myRender = GetComponent<Renderer>();
         theRigidBody = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //calculate distance from player
         fpsTargetDistance = Vector3.Distance(fpsTarget.position, transform.position);
+        //if player is within attack distance
         if (fpsTargetDistance < attackDistance)
         {
+            //change color and attack
             myRender.material.color = Color.red;
             lookAtPlayer();
             attackPlease();
             print("ATTACK");
         }
-        
+        //if player within look distance
         else if (fpsTargetDistance < enemyLookDistance)
         {
+            //change color and look at player
             myRender.material.color = Color.yellow;
-           // lookAtPlayer();
-          //  print("Look At Player Please ");
+            lookAtPlayer();
         }
         else
         {
+            //change color
             myRender.material.color = Color.blue;
         }
+        //if just respawned and spawn too close
+        if (fpsTargetDistance < 20 && didRespawn)
+        {
+            //spawan again and print to console
+            print("TOO CLOSE");
+            respawnEnemy();
+        }
+        else
+        {
+            //good respawn distance, dont respawn again until collision
+            print("SAFE DIDSTANCE");
+            didRespawn = false;
+        }
 
-        
-	}
+
+    }
+    //look at player function
     void lookAtPlayer()
     {
         Quaternion rotation = Quaternion.LookRotation(fpsTarget.position - transform.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime+damping);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime + damping);
     }
+    //attack player function
     void attackPlease()
     {
-        theRigidBody.transform.forward.Set(0,0,1);
-        theRigidBody.AddForce(transform.forward*enemyMovementSpeed);
+        theRigidBody.transform.forward.Set(0, 0, 1);
+        theRigidBody.AddForce(transform.forward * enemyMovementSpeed);
+    }
+    //collison detection
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "PlayerMain")
+        {
+            respawnEnemy();
+        }
+    }
+    //repsawn function
+    private void respawnEnemy()
+    {
+        //declaring respawn vars and setting random values
+        Vector3 respawn;
+        int randX = Random.Range(50, 400);
+        int randY = Random.Range(1, 2);
+        int randZ = Random.Range(50, 400);
+        //putting random values into the Vector3 respawn item
+        respawn.x = randX;
+        respawn.y = randY;
+        respawn.z = randZ;
+
+        /* working on respawn distance checker
+        do
+        {
+            distance = Vector3.Distance(player.position, transform.position);
+            randX = Random.Range(50, 400);
+            randZ = Random.Range(50, 400);
+            respawn.x = randX;
+            respawn.z = randZ;
+        } while (distance < 20);
+        */
+        //respawn, look at player, print to console to say player hit and also set bool to true for repsawn distance checking
+        transform.position = respawn;
+        transform.rotation = fpsTarget.rotation;
+        print("Player hit!");
+        didRespawn = true;
     }
 }
