489f9216ddbcd43969367f9563018d36ba5d358e
Joe <Prochaska.joseph@gmail.com>
2017-03-20 22:36:13 -0400

    Working on respawn
    working on respawn for enemy AI

--- Assets/Scripts/OnCollison.cs
+++ Assets/Scripts/OnCollison.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OnCollison : MonoBehaviour {
-    public Vector3 respawn;
 
+    public Transform player;
     private void OnCollisionEnter(Collision collision)
     {
+        float distance;
+        Vector3 respawn;
+        int randX = Random.Range(50, 400);
+        int randY = Random.Range(2, 4);
+        int randZ = Random.Range(50, 400);
+
+        respawn.x = randX;
+        respawn.y = randY;
+        respawn.z = randZ;
+
+        distance = Vector3.Distance(player.position, transform.position);
 
         if (collision.gameObject.name == "PlayerMain" )
         {
+            /* working on respawn distance checker
+            do
+            {
+                distance = Vector3.Distance(player.position, transform.position);
+                randX = Random.Range(50, 400);
+                randZ = Random.Range(50, 400);
+                respawn.x = randX;
+                respawn.z = randZ;
+            } while (distance < 20);
+            */
+
             transform.position = respawn;
             print("Player hit!");
         }
