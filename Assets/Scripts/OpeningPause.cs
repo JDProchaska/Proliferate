5a52b834f0260cb60c3d102564e671d6f53ae434
Joe <Prochaska.joseph@gmail.com>
2017-03-25 00:20:23 -0400

    Fixed animation
    Fixed animation from constanly playing in background.

--- Assets/Scripts/OpeningPause.cs
+++ Assets/Scripts/OpeningPause.cs
@@ -24,6 +24,7 @@ public class OpeningPause : MonoBehaviour {
             enemy.GetComponent<AIEnemy>().enabled = true;
             cutsceneCamera.GetComponent<AudioListener>().enabled = false;
             UI.SetActive(true);
+            cutsceneCamera.GetComponent<Animator>().Stop();
             GetComponent<OpeningPause>().enabled = false;
         }
         else
